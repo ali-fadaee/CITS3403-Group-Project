b38c87f47fb343f3b4d6a2c1eb0c36bc57ee04bc
Mathew Libby <mathew@considerit.com.au>
2026-03-30 15:56:37 +0800

    add filter fields, my debates button

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 
 def create_app():
@@ -7,11 +7,12 @@ def create_app():
     @app.route('/')
     def index():
         debates = [
-            {'title': 'Is AI a threat to employment?', 'tags': ['Technology', 'Science'], 'yes_count': 3, 'no_count': 1},
-            {'title': 'Should social media be regulated?', 'tags': ['Politics', 'Technology'], 'yes_count': 2, 'no_count': 8},
-            {'title': 'Am I a horrible perosn for cheating on my wife?', 'tags': ['Lifestyle', 'Relationships'], 'yes_count': 10, 'no_count': 2},
+            {'title': 'Is AI a threat to employment?', 'tags': ['Technology', 'Science'], 'yes_count': 3, 'no_count': 1, 'created': '28 Mar 2026', 'last_activity': '30 Mar 2026'},
+            {'title': 'Should social media be regulated?', 'tags': ['Politics', 'Technology'], 'yes_count': 2, 'no_count': 8, 'created': '25 Mar 2026', 'last_activity': '29 Mar 2026'},
+            {'title': 'Am I a horrible person for cheating on my wife?', 'tags': ['Lifestyle', 'Relationships'], 'yes_count': 10, 'no_count': 2, 'created': '20 Mar 2026', 'last_activity': '30 Mar 2026'},
         ]
-        return render_template('index.html', debates=debates)
+        filter = request.args.get('filter', 'new')
+        return render_template('index.html', debates=debates, filter=filter)
 
     @app.route('/login')
     def login():
