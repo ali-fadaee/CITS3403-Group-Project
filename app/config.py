06849a1c5926037b1d2d230b8f3ca4cc7b4515dc
MathewLibby <mathew@considerit.com.au>
2026-05-05 01:33:29 +0800

    Added SMTP credentials template to .env.example, and imported credentials from .env in config.py

--- app/config.py
+++ app/config.py
@@ -6,4 +6,10 @@ default_db_path = "sqlite:///" + os.path.join(basedir, "app.db")
 
 class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL") or default_db_path
-    SECRET_KEY = os.getenv("SECRET_KEY") 
+    SECRET_KEY = os.getenv("SECRET_KEY")
+    MAIL_SERVER = os.getenv("MAIL_SERVER", "sandbox.smtp.mailtrap.io")
+    MAIL_PORT = int(os.getenv("MAIL_PORT", 2525))
+    MAIL_USERNAME = os.getenv("MAIL_USERNAME")
+    MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
+    MAIL_USE_TLS = True
+    MAIL_USE_SSL = False
